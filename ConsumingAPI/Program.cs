8682d2e1369f5b6fe3e9b612babd25c5ac1da269
alexandrem1 <alee_ppl@hotmail.com>
2018-03-19 11:15:33 -0300

    Add project files.

---
+++ ConsumingAPI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsumingAPI.Sync;
+
+namespace ConsumingAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumeAPI objsync = new ConsumeAPI();
+            objsync.GetAllEventData(); //Adding Event
+        }
+    }
+}
---
+++ ConsumingAPI/Sync.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumingAPI
+{
+    public class Sync
+    {
+        public class ConsumeAPI
+        {
+            public void GetAllEventData() //Get All Events Records  
+            {
+                using (var client = new WebClient()) //WebClient  
+                {
+                    while (true)
+                    {
+                    client.Headers.Add("Content-Type:application/json"); //Content-Type  
+                    client.Headers.Add("Accept:application/json");
+                    var result = client.DownloadString("http://localhost:63312/api/usuario/ConsultarUsuarios"); //URI
+                    Console.WriteLine(Environment.NewLine + result);
+                    Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
